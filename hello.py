96e04c230ab46c7ca546574603e8b2a4711b61a0
Janusz Martyniak <martynia@heppc201.hep.ph.ic.ac.uk>
2017-09-11 14:58:59 +0100

    hello, first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+
+print "----------------------"
+print "Hello, I'm a snake !"
+print "----------------------"
+
+print " More info:\n"
+
+print (sys.version)
+
+#
+
